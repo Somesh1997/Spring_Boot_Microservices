a6f75c4aad596410080054d4c52884a916c3aa68
Somesh1997 <somesh6g@gmail.com>
2021-10-02 17:24:06 +0530

    first commit

--- contactService/src/main/java/com/contacts/service/ContactService.java
+++
@@ -1,9 +0,0 @@
-package com.contacts.service;
-import java.util.List;
-
-import com.contacts.entity.Contact;
-public interface ContactService {
-	
-	public List<Contact> getContactsOfUser(Long userId);
-
-}
